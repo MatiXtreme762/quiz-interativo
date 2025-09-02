f059897c3266067e9b84cfada6f93db7188785a5
MatiXtreme762 <bilovus.matheus@escola.pr.gov.br>
2025-09-02 11:04:36 -0300

    .

--- js/script.js
+++ js/script.js
@@ -4,8 +4,8 @@ import {perguntas} from './perguntas.js';
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
-const caixaReultado = document.querySelector(".caixa-resultado");
-const botaoReniciar = document.querySelector(".iniciar-btn");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const botaoIniciar = document.querySelector(".iniciar-btn");
 const telaInicial = document.querySelector(".tela-inicial");
 
 let atual = 0;
